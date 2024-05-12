e6a7b430a0d602ecd9fa8f55388c0798c111549e
shirinshapoori27@gmail.com <beh48@Behpouyan.local>
2024-05-12 12:33:42 +0330

    personalInfo Update Sub menu

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,45 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import { toPersianDigit } from "./toPersianDigit";
 
-const PersonalInfo = ({item , id}) => {
-    const info=[{id:0, type:"نام و نام‌خانوادگی", name:"شیرین"},]
-  return (<>
-    <div className='personal-info' >
-        {id === 0 ? 
-         (<div onClick={()=> openFiled(id)}>
-         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="#FF5CAE" fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0a4.5 4.5 0 0 1-9 0M3.751 20.105a8.25 8.25 0 0 1 16.498 0a.75.75 0 0 1-.437.695A18.7 18.7 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695" clip-rule="evenodd"/></svg>
-         </div>): null
-        } 
-        {id === 1 ? (<div onClick={()=> openFiled(id)}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="#FF842C" d="M19 6H5a3 3 0 0 0-3 3v2.72L8.837 14h6.326L22 11.72V9a3 3 0 0 0-3-3" opacity="0.5"/><path fill="#FF842C" d="M10 6V5h4v1h2V5a2.002 2.002 0 0 0-2-2h-4a2.002 2.002 0 0 0-2 2v1zm-1.163 8L2 11.72V18a3.003 3.003 0 0 0 3 3h14a3.003 3.003 0 0 0 3-3v-6.28L15.163 14z"/></svg>
-       </div> ): null
-        } 
-        {id === 2 ?
-        (<div onClick={()=> openFiled(id)}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="#ff3d4f" d="M12 17a2 2 0 0 0 2-2a2 2 0 0 0-2-2a2 2 0 0 0-2 2a2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2zm-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3"/></svg>
-       </div> ): null
-        }  
-        {id === 3 ?
-        (<div onClick={()=> openFiled(id)}>
-         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="#3BBFDC" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="18" cy="15" r="3"/><circle cx="9" cy="7" r="4"/><path d="M10 15H6a4 4 0 0 0-4 4v2m19.7-4.6l-.9-.3m-5.6-2.2l-.9-.3m2.3 5.1l.3-.9m2.2-5.6l.3-.9m.2 7.4l-.4-1m-2.4-5.4l-.4-1m-2.1 5.3l1-.4m5.4-2.4l1-.4"/></g></svg>
-        </div>) : null
-          }    
-        <p className='long-txt'>{item.name}</p>
-         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="#000" d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z"/></svg>
-      </div>
-  </>)
-}
-export default PersonalInfo
+const PersonalInfo = ({ item, subItems, id }) => {
+  const [open, setOpen] = useState(false);
+  const info = [{ type: "نام و نام‌خانوادگی", name: "شیرین شاپوری "}, {type: "شماره موبایل", name: "09151234567"}
+  , {type:"تاریخ تولد" , name:"1366/12/1" },{ type:"کد پرسنلی", name:"7612345"}];
+ 
+  const [select, setSelect] = useState(false);
 
-function openFiled({id}){
-  return(
-    <>
-    {id === 0 ? (<div className='more-info'>00000</div>) : null}
-    {id === 1 ? (<div className='more-info'>11111</div>) : null}
-    {id === 2 ? (<div className='more-info'>2222222</div>) : null}
-    {id === 3 ? (<div className='more-info'>33333333</div>) : null}
-    </>
-  )
- }
+  return (
+    <div className="personal-info">
+      <div className="personal-titr" onClick={() => setOpen(!open)}>
+        {item.icon}
+        <p className="long-txt">{item.name}</p>
+         {open ? <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="#000" d="m488.832 344.32l-339.84 356.672a32 32 0 0 0 0 44.16l.384.384a29.44 29.44 0 0 0 42.688 0l320-335.872l319.872 335.872a29.44 29.44 0 0 0 42.688 0l.384-.384a32 32 0 0 0 0-44.16L535.168 344.32a32 32 0 0 0-46.336 0"/></svg> 
+         : <svg xmlns="http://www.w3.org/2000/svg"
+             idth="1em"  height="1em"viewBox="0 0 1024 1024"><path fill="#000" d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z"
+        /></svg>} 
+                  </div>
+                  {open ? (<> 
+                       {subItems && subItems.map((item) => (
+                           <>
+                           { id === 2 ? <input placeholder={item.type} className="input-pass"></input> : <>
+                           <div className='more-info'>
+                           <p className="long-txt">{item.type}</p>
+                            {id < 2 ? <p className="long-txt" style={{display:"flex", justifyContent:"flex-end"}}>{typeof item.name === 'string' ? item.name : `${toPersianDigit(item.name)}`}</p>
+                           :  <div style={{marginTop:"5px"}}>{item.icon}</div>
+                          }
+                           </div>   </> }
+                           {/* { id === 3 ? <div onClick={setSelect(!select)}></div> : null} */}
+                             </>
+                       ))}
+
+            </>
+              ) : (   
+                    null    
+                )}
+      
+      {open && (
+        <></>
+      )}
+  </div>
+  );
+};
+export default PersonalInfo;
+
 //     const MorePersonalInfo= () => {
 //          return(
 //            <div className='more-info'>
@@ -53,14 +58,14 @@ function openFiled({id}){
 //          f
 //            </div>
 //          )
-//        } 
+//        }
 //        const SecurityInfo= () => {
 //          return(
 //            <div className='more-info'>
 //             ssss
 //            </div>
 //          )
-//        } 
+//        }
 //        const RoleInfo= () => {
 //          return(
 //            <div className='more-info'>
@@ -68,4 +73,3 @@ function openFiled({id}){
 //            </div>
 //          )
 //        }
- 
--- src/pages/PersonalInfoPage.js
+++ src/pages/PersonalInfoPage.js
@@ -1,57 +1,248 @@
-import React, { useState } from 'react'
-import PersonalInfo from '../components/PersonalInfo';
+import React, { useState } from "react";
+import PersonalInfo from "../components/PersonalInfo";
 
 const PersonalInfoPage = () => {
-  
-   const items = ([
-    { name: "مشخصات فردی" ,isSelected:false },
-    { name: "سوابق تحصيلی و شغلی" ,isSelected:false  },
-    { name: "امنیت" ,isSelected:false  },
-    { name: "سمت شغلی" ,isSelected:false  },
-  ]);
-         
-      // function getOnClick({index}) {
-      //             if(index === 0) {
-      //                 <MorePersonalInfo />
-      //             }
-      //             else if(index === 1) {
-      //                 <EducationalInfo />
-      //             }  
-      //            else if(index === 2) {
-      //                 <SecurityInfo />
-      //             }
-      //             else
-      //               return( <RoleInfo /> 
-      //               )
-                  // switch(index) {
-                  // case 0:
-                  //  return console.log("fffffffffff");
-                  // case 1:
-                  //   return <EducationalInfo />;
-                  // case 2:
-                  //   return <SecurityInfo />;
-                  // default:
-                  //   return <RoleInfo />;  
-                  //    }
-                  
+  const items = [
+    {
+      name: "مشخصات فردی",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#FF5CAE"
+            fill-rule="evenodd"
+            d="M7.5 6a4.5 4.5 0 1 1 9 0a4.5 4.5 0 0 1-9 0M3.751 20.105a8.25 8.25 0 0 1 16.498 0a.75.75 0 0 1-.437.695A18.7 18.7 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695"
+            clip-rule="evenodd"
+          />
+        </svg>
+      ),
+
+      subItems: [
+        {
+          type:"نام و نام خانوادگی",
+          name: "شیرین شاپوری",
+        },
+        {
+          type:"شماره موبایل",
+          name: 9121234676,
+        },
+        {
+          type:"تاریخ تولد",
+           name: 1379,
+        }, 
+        {
+          type:"کد ملی",
+           name: 925252552 ,
+        }, 
+        {
+          type:"کد پرسنلی",
+           name: 37412,
+        }
+      ],
+    },
+    {
+      name: "سوابق تحصيلی و شغلی",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#FF842C"
+            d="M19 6H5a3 3 0 0 0-3 3v2.72L8.837 14h6.326L22 11.72V9a3 3 0 0 0-3-3"
+            opacity="0.5"
+          />
+          <path
+            fill="#FF842C"
+            d="M10 6V5h4v1h2V5a2.002 2.002 0 0 0-2-2h-4a2.002 2.002 0 0 0-2 2v1zm-1.163 8L2 11.72V18a3.003 3.003 0 0 0 3 3h14a3.003 3.003 0 0 0 3-3v-6.28L15.163 14z"
+          />
+        </svg>
+      ),
+       subItems: [
+         {
+          type:"مدرك تحصيلی",
+          name: "لیسانس",
+          isString:false,
+         },
+         {
+          type:"رشته تحصيلی",
+          name: "نرم افزار",
+          isString:false,
+         },
+         {
+          type:"سابقه تدریس",
+           name: 22,
+          isString:false,
+         }, 
+         {
+          type:"مدرک تحصیلی",
+           name: "File.png",
+          isString:false,
+         }, 
+          {
+          type:"گواهینامه‌های اخذ شده",
+           name: "File.png",
+          isString:false,
+         }
+      ],
+    },
+    {
+      name: "امنیت",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#ff3d4f"
+            d="M12 17a2 2 0 0 0 2-2a2 2 0 0 0-2-2a2 2 0 0 0-2 2a2 2 0 0 0 2 2m6-9a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10a2 2 0 0 1 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2zm-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3"
+          />
+        </svg>
+      ),
+      subItems: [
+         {
+          type:"رمز فعلی",
+           name: 1234,
+         }, 
+         {
+          type:"رمز جدید",
+           name: "|",
+         }, 
+          {
+          type:"تکرار رمز جدید",
+           name: "|",
+         }
+      ],
+    },
+    {
+      name: "سمت شغلی",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
+          <g
+            fill="none"
+            stroke="#3BBFDC"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <circle cx="18" cy="15" r="3" />
+            <circle cx="9" cy="7" r="4" />
+            <path d="M10 15H6a4 4 0 0 0-4 4v2m19.7-4.6l-.9-.3m-5.6-2.2l-.9-.3m2.3 5.1l.3-.9m2.2-5.6l.3-.9m.2 7.4l-.4-1m-2.4-5.4l-.4-1m-2.1 5.3l1-.4m5.4-2.4l1-.4" />
+          </g>
+        </svg>
+      ),
+       subItems: [
+        {
+          type:"مدرسه",
+          icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="90%"
+            height="1em"
+            viewBox="0 0 1024 1024"
+          >
+            <path
+              fill="#000"
+              d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z" />
+            </svg>
+            ),
+           name: "دخترانه شهید حاتمی",
+         }, 
+         {
+          type:"نقش",
+            icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="90%"
+            height="1em"
+            viewBox="0 0 1024 1024"
+          >
+            <path
+              fill="#000"
+              d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z" />
+            </svg>
+            ),
+           name: "معلم",
+         }, 
+          {
+          type:"ساعت ابلاغ",
+            icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="90%"
+            height="1em"
+            viewBox="0 0 1024 1024"
+          >
+            <path
+              fill="#000"
+              d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z" />
+            </svg>
+            ),
+           name: "6 ساعت",
+         } ,
+         {
+          type:"تدریس تخصصی",
+            icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="90%"
+            height="1em"
+            viewBox="0 0 1024 1024"
+          >
+            <path
+              fill="#000"
+              d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z" />
+            </svg>
+            ),
+           name: "ریاضی",
+         },
+         {
+          type:"تدریس غیرتخصصی",
+            icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="90%"
+            height="1em"
+            viewBox="0 0 1024 1024"
+          >
+            <path
+              fill="#000"
+              d="M831.872 340.864L512 652.672L192.128 340.864a30.59 30.59 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.59 30.59 0 0 0-42.752 0z" />
+            </svg>
+            ),
+           name: "هنر ",
+         },
+      ],
+    },
+  ];
 
   return (
-    <div className='profile-border'>
-       {items.map((item, index) => (
-        <PersonalInfo item={item} id={index}  />
-     ))}
-      
-       {/* let isOpen= items.map((item,i) => {
-            if(i === index) {
-                return  {...item, isSelected : !item.isSelected}
-            }
-            return item;
-            <PersonalInfo item={item} onClick={setItems()}/>
-        );} */}
+    <div className="profile-border">
+      {items.map((item,index) => (
+        <PersonalInfo item={item} subItems={item.subItems} id={index}/>
+      ))}
     </div>
-  )
-}
-export default PersonalInfoPage 
+  );
+};
+export default PersonalInfoPage;
+
+
+
+
+
 
 // function profilleIcon(index) {
 //     switch (index) {
@@ -65,3 +256,29 @@ export default PersonalInfoPage
 //           <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="#3BBFDC" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="18" cy="15" r="3"/><circle cx="9" cy="7" r="4"/><path d="M10 15H6a4 4 0 0 0-4 4v2m19.7-4.6l-.9-.3m-5.6-2.2l-.9-.3m2.3 5.1l.3-.9m2.2-5.6l.3-.9m.2 7.4l-.4-1m-2.4-5.4l-.4-1m-2.1 5.3l1-.4m5.4-2.4l1-.4"/></g></svg>        return ;
 //     }
 //    }
+
+
+
+  // function getOnClick({index}) {
+  //             if(index === 0) {
+  //                 <MorePersonalInfo />
+  //             }
+  //             else if(index === 1) {
+  //                 <EducationalInfo />
+  //             }
+  //            else if(index === 2) {
+  //                 <SecurityInfo />
+  //             }
+  //             else
+  //               return( <RoleInfo />
+  //               )
+  // switch(index) {
+  // case 0:
+  //  return console.log("fffffffffff");
+  // case 1:
+  //   return <EducationalInfo />;
+  // case 2:
+  //   return <SecurityInfo />;
+  // default:
+  //   return <RoleInfo />;
+  //    }
